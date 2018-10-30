42c5b74510b413521c81b2c11dcb90b33af00603
Rona Tsai <ronatsai@RonadeMacBook-Air.local>
2018-10-30 19:34:02 +0800

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Rona Tsai on 2018/10/30.
+//  Copyright © 2018 Rona Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
